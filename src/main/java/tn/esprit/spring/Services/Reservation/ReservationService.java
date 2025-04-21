3c9b31fc6294fc8cc84bac5469f51166d9ad690f
alakhal19 <amine.lakhal@esprit.tn>
2025-04-21 20:30:44 +0200

    remove extra modules

--- src/main/java/tn/esprit/spring/Services/Reservation/ReservationService.java
+++ src/main/java/tn/esprit/spring/Services/Reservation/ReservationService.java
@@ -3,10 +3,8 @@ package tn.esprit.spring.Services.Reservation;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import tn.esprit.spring.DAO.Entities.Chambre;
 import tn.esprit.spring.DAO.Entities.Etudiant;
 import tn.esprit.spring.DAO.Entities.Reservation;
-import tn.esprit.spring.DAO.Repositories.ChambreRepository;
 import tn.esprit.spring.DAO.Repositories.EtudiantRepository;
 import tn.esprit.spring.DAO.Repositories.ReservationRepository;
 
