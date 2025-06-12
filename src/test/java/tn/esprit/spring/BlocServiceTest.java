d9d96c4569366c4e880dde1e9aa2800238d5145b
RouissiZouhour <zouhour.rouissi@esprit.tn>
2025-06-12 02:05:09 +0100

    none

--- src/test/java/tn/esprit/spring/BlocServiceTest.java
+++ src/test/java/tn/esprit/spring/BlocServiceTest.java
@@ -1,113 +1,63 @@
 package tn.esprit.spring;
 
-import org.junit.After;
 import org.junit.jupiter.api.*;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-import tn.esprit.spring.DAO.Entities.Bloc;
-import tn.esprit.spring.DAO.Entities.Chambre;
-import tn.esprit.spring.DAO.Entities.Foyer;
-import tn.esprit.spring.DAO.Repositories.BlocRepository;
-import tn.esprit.spring.DAO.Repositories.ChambreRepository;
-import tn.esprit.spring.DAO.Repositories.FoyerRepository;
-import tn.esprit.spring.Services.Bloc.BlocService;
+import tn.esprit.spring.DAO.Entities.Universite;
+import tn.esprit.spring.Services.Bloc.IBlocService;
+import tn.esprit.spring.Services.Universite.IUniversiteService;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.TestMethodOrder;
 
-@RunWith(SpringRunner.class)
-@TestMethodOrder(MethodOrderer.class)
+import java.util.List;
+
+@TestMethodOrder(OrderAnnotation.class)
 @SpringBootTest
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class BlocServiceTest {
-    @Autowired
-    BlocService blocService;
 
     @Autowired
-    BlocRepository blocRepository;
+    IBlocService blocService;
 
-    @Autowired
-    ChambreRepository chambreRepository;
+    private static Long savedBlocId;
 
-    @Autowired
-    FoyerRepository foyerRepository;
 
-    private Bloc testBloc;
-    private Foyer testFoyer;
-
-    @BeforeEach
-    void beforeEach() {
-        // Créer un foyer
-        testFoyer = Foyer.builder()
-                .nomFoyer("Foyer Central")
-                .capaciteFoyer(100L)
-                .build();
-        foyerRepository.save(testFoyer);
-
-        // Créer un bloc
-        testBloc = Bloc.builder()
-                .nomBloc("Bloc A")
-                .capaciteBloc(30L)
-                .foyer(testFoyer)
-                .build();
-        blocRepository.save(testBloc);
-
-        // Ajouter des chambres
-        Chambre ch1 = Chambre.builder()
-                .numeroChambre(101)
-                .bloc(testBloc)
-                .build();
-
-        Chambre ch2 = Chambre.builder()
-                .numeroChambre(102)
-                .bloc(testBloc)
-                .build();
-
-        chambreRepository.save(ch1);
-        chambreRepository.save(ch2);
-    }
-
-    @AfterEach
-    void afterEach() {
-        chambreRepository.deleteAll();
-        blocRepository.deleteAll();
-        foyerRepository.deleteAll();
+    @AfterAll
+    public void afterAll(){
+        blocService.deleteById(savedBlocId);
+        Universite u = blocService.findById(savedBlocId);
+        System.out.println(u.toString());
+        Assertions.assertTrue( u.getNomUniversite().equals("mouch maoujoud"));
     }
 
     @Test
     @Order(1)
-    void testAddBloc() {
-        Bloc bloc = Bloc.builder()
-                .nomBloc("Bloc B")
-                .capaciteBloc(40L)
-                .foyer(testFoyer)
+    public void testAddOrUpdate() {
+        Universite u = Universite.builder()
+                .nomUniversite("ESPRIT")
+                .adresse("Ariana")
                 .build();
+        System.out.println("object   "+u);
+        Universite saved = universiteService.addOrUpdate(u);
+        System.out.println("saaaaaaved   "+saved);
+        savedUniversiteId = saved.getIdUniversite();
 
-        Bloc savedBloc = blocService.ajouterBlocEtSesChambres(bloc);
-        Assertions.assertNotNull(savedBloc.getIdBloc());
-        Assertions.assertEquals("Bloc B", savedBloc.getNomBloc());
+        Assertions.assertNotNull(saved.getIdUniversite());
+        Assertions.assertEquals("ESPRIT", saved.getNomUniversite());
     }
 
     @Test
     @Order(2)
-    void testFindBlocById() {
-        Bloc bloc = blocRepository.findById(testBloc.getIdBloc()).orElse(null);
-        Assertions.assertNotNull(bloc);
-        Assertions.assertEquals("Bloc A", bloc.getNomBloc());
+    public void testFindAll() {
+        List<Universite> list = universiteService.findAll();
+        Assertions.assertFalse(list.isEmpty());
     }
 
     @Test
     @Order(3)
-    void testUpdateBloc() {
-        testBloc.setNomBloc("Bloc A+");
-        Bloc updated = blocService.addOrUpdate(testBloc);
-        Assertions.assertEquals("Bloc A+", updated.getNomBloc());
+    public void testFindById() {
+        Universite u = universiteService.findById(savedUniversiteId);
+        Assertions.assertEquals("ESPRIT", u.getNomUniversite());
     }
-
-    @Test
-    @Order(4)
-    void testDeleteBloc() {
-        blocService.deleteById(testBloc.getIdBloc());
-        Assertions.assertFalse(blocRepository.findById(testBloc.getIdBloc()).isPresent());
-    }
-
 
 }
